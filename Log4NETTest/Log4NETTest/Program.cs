e50dc160b6fcc3b31e665a883ec9cc8f105fe782
kalkusar <nihilist@10g.pl>
2013-10-05 23:35:32 +0200

    First glance at log4net.

--- Log4NETTest/Log4NETTest/Program.cs
+++ Log4NETTest/Log4NETTest/Program.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Web;
 using log4net;
+using log4net.Config;
 
 namespace Log4NETTest
 {
     class Program
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
         static void Main(string[] args)
         {
-
+            var fi = new FileInfo("..\\..\\Config\\Log4NETConfig.xml");
+            XmlConfigurator.Configure(fi);
+            log.Info("Test log.");
+            log.Debug("Test dbg log.");
         }
     }
 }
